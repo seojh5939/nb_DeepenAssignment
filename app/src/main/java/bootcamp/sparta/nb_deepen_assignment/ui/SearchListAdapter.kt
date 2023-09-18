f8659fa0201e2b5fb5b03435252f3d6e1b0e6797
SeoJungHan <seojh5939@gmail.com>
2023-09-18 21:38:04 +0900

    [FEAT] : ⚙️ RecyclerView ListAdapter 데이터 바인딩 && DTTM 포멧 변경 후 View 출

--- app/src/main/java/bootcamp/sparta/nb_deepen_assignment/ui/SearchListAdapter.kt
+++ app/src/main/java/bootcamp/sparta/nb_deepen_assignment/ui/SearchListAdapter.kt
@@ -1,15 +1,28 @@
 package bootcamp.sparta.nb_deepen_assignment.ui
 
+import android.app.Activity
+import android.content.Context
+import android.content.res.Resources
 import android.net.Uri
+import android.os.Build
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.core.view.marginStart
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import bootcamp.sparta.nb_deepen_assignment.R
 import bootcamp.sparta.nb_deepen_assignment.databinding.RecyclerItemBinding
 import bootcamp.sparta.nb_deepen_assignment.model.ContentData
+import com.bumptech.glide.Glide
+import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+import java.util.TimeZone
 
-class SearchListAdapter(): ListAdapter<ContentData, SearchListAdapter.SearchHolder>(
+class SearchListAdapter(private val context: Context): ListAdapter<ContentData, SearchListAdapter.SearchHolder>(
     object: DiffUtil.ItemCallback<ContentData>() {
         override fun areItemsTheSame(oldItem: ContentData, newItem: ContentData): Boolean {
             return oldItem.id == newItem.id
@@ -20,15 +33,31 @@ class SearchListAdapter(): ListAdapter<ContentData, SearchListAdapter.SearchHold
         }
     }
 ) {
-    class SearchHolder(private val binding: RecyclerItemBinding): RecyclerView.ViewHolder(binding.root) {
-        fun bind(item: ContentData)=with(binding){
-            // TODO 통신처리 후 받아온 url 어떻게 처리할지 결정후 변경
-            image.setImageURI(Uri.parse(item.thumbnail))
+
+    class SearchHolder(private val binding: RecyclerItemBinding, private val context: Context): RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: ContentData)=with(binding) {
+            Log.d("Adapter", item.dateTime)
+            val old_format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.000Z", Locale.KOREAN)
+            val old_date = old_format.parse(item.dateTime)
+            val formatter = SimpleDateFormat("yyyy-MM-dd", Locale.KOREAN)
+            val new_date = old_date?.let { formatter.format(it) }
+
+            dttm.text = new_date
+            Glide.with(context)
+                .load(item.thumbnail)
+                .into(image)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchHolder {
-        return SearchHolder(RecyclerItemBinding.inflate(LayoutInflater.from(parent.context)))
+        val view = RecyclerItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+//        val layoutParams = RecyclerView.LayoutParams(
+//            ViewGroup.LayoutParams.MATCH_PARENT,
+//            ViewGroup.LayoutParams.WRAP_CONTENT
+//        )
+//        parent.rootView.layoutParams = layoutParams
+//        parent.rootView.setPadding(10, 10, 10, 10dp)
+        return SearchHolder(view, context)
     }
 
     override fun onBindViewHolder(holder: SearchHolder, position: Int) {
