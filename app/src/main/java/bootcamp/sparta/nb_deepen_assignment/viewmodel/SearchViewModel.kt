68041632047583b44b291913e61725cc572d6599
SeoJungHan <seojh5939@gmail.com>
2023-09-20 17:23:37 +0900

    [FEAT] : ⚙️ 좋아요 구현완

--- app/src/main/java/bootcamp/sparta/nb_deepen_assignment/viewmodel/SearchViewModel.kt
+++ app/src/main/java/bootcamp/sparta/nb_deepen_assignment/viewmodel/SearchViewModel.kt
@@ -5,68 +5,45 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import bootcamp.sparta.nb_deepen_assignment.model.ContentData
-import bootcamp.sparta.nb_deepen_assignment.model.retrofit.ImageData
-import bootcamp.sparta.nb_deepen_assignment.model.retrofit.ImageSearchResponse
 import bootcamp.sparta.nb_deepen_assignment.model.retrofit.VideoData
-import bootcamp.sparta.nb_deepen_assignment.model.retrofit.VideoSearchResponse
 import bootcamp.sparta.nb_deepen_assignment.repository.ContentRepository
 import kotlinx.coroutines.launch
-import retrofit2.Response
+import java.util.concurrent.atomic.AtomicLong
 
-class MainViewModel(private val repository: ContentRepository): ViewModel(){
-    val contentList: MutableLiveData<List<ContentData>> = MutableLiveData()
+class SearchViewModel(private val repository: ContentRepository): ViewModel(){
+    private val _list: MutableLiveData<List<ContentData>> = MutableLiveData()
+    val list get() = _list
 
     fun resultImageAndVideo(query: String) {
         viewModelScope.launch {
-            val list: MutableList<ContentData> = mutableListOf()
-            val imageRes= repository.searchImage(query, "recency")
-            val videoRes= repository.searchVideo(query, "recency")
-
-            if(imageRes.isSuccessful && videoRes.isSuccessful) {
-                imageRes.body()?.documents?.let {imageList ->
-                    videoRes.body()?.documents?.let { videoList ->
-                        list.addAll(videoList.toContentDataList())
-                        list.addAll(imageList.toContentDataList())
-                        list.sortWith(Comparator { o1, o2 -> o2.dateTime.compareTo(o1.dateTime) })
-                        contentList.value = list
-                    }
-                }
-            }
+            val result = repository.resultImageAndVideo(query, "recency")
+            _list.value = result
         }
     }
 
-    @JvmName("toContentDataListForImage")
-    private fun MutableList<ImageData>.toContentDataList(): MutableList<ContentData> {
-        val list: MutableList<ContentData> = mutableListOf()
-        for(i in this.indices) {
-            val converter = ContentData(
-                id = list.size,
-                dateTime = this[i].dateTime,
-                thumbnail = this[i].thumbnail
-            )
-            list.add(converter)
-        }
-        return list
-    }
+    fun modifySearchItem(position: Int?= null, item: ContentData) {
+        fun searchIndex(item: ContentData): Int {
+            val currentList = list.value.orEmpty().toMutableList()
+            val selectedItem = currentList.find { it.id == item.id }
 
-    @JvmName("toContentDataListForVideo")
-    private fun MutableList<VideoData>.toContentDataList(): MutableList<ContentData> {
-        val list: MutableList<ContentData> = mutableListOf()
-        for(i in this.indices) {
-            val converter = ContentData(
-                id = list.size,
-                dateTime = this[i].dateTime,
-                thumbnail = this[i].thumbnail
-            )
-            list.add(converter)
+            return selectedItem?.let { currentList.indexOf(it) } ?: return -1
         }
-        return list
+
+        val currentList = list.value.orEmpty().toMutableList()
+        val currentPosition = position ?: searchIndex(item)
+
+        if(currentPosition == -1) {
+            return
+        }
+
+        currentList[currentPosition] = item
+        _list.value = currentList
     }
 }
 
 class MainViewModelFactory(private val repository: ContentRepository): ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         @Suppress("UNCHECKED_CAST")
-        return MainViewModel(repository) as T
+        return SearchViewModel(repository) as T
     }
 }
