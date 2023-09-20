e6245981f0596657e45f0f715b77bf6f9f4fa852
SeoJungHan <seojh5939@gmail.com>
2023-09-20 17:22:31 +0900

    [FEAT] : ⚙️ Util 생성 및 Date Format 변경기능추

--- app/src/main/java/bootcamp/sparta/nb_deepen_assignment/utils/Utils.kt
+++ app/src/main/java/bootcamp/sparta/nb_deepen_assignment/utils/Utils.kt
@@ -1,4 +1,16 @@
 package bootcamp.sparta.nb_deepen_assignment.utils
 
+import java.text.SimpleDateFormat
+import java.util.Locale
+
 object Utils {
+    // 날짜 포멧변경
+    fun changeDateFormat(dttm: String): String {
+        val old_format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.000Z", Locale.KOREAN)
+        val old_date = old_format.parse(dttm)
+        val formatter = SimpleDateFormat("yyyy-MM-dd", Locale.KOREAN)
+        val new_date = old_date?.let { formatter.format(it) }
+
+        return new_date ?: "invalid Format"
+    }
 }
