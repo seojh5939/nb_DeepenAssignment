a1aaf73dca551571117fda94cd266e6dbf7b61af
SeoJungHan <seojh5939@gmail.com>
2023-09-20 17:16:32 +0900

    [FEAT] : ⚙️ SharedViewModel init

--- app/src/main/java/bootcamp/sparta/nb_deepen_assignment/ui/main/MainSharedViewModel.kt
+++ app/src/main/java/bootcamp/sparta/nb_deepen_assignment/ui/main/MainSharedViewModel.kt
@@ -1,4 +1,37 @@
 package bootcamp.sparta.nb_deepen_assignment.ui.main
 
-class MainsharedViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import bootcamp.sparta.nb_deepen_assignment.model.ContentData
+
+class MainSharedViewModel: ViewModel(){
+    private val _searchedEvent: MutableLiveData<MainSharedEventForSearch> = MutableLiveData()
+    val searchedEvent get() = _searchedEvent
+
+    private val _myPageEvent: MutableLiveData<MainSharedEventForMyPage> = MutableLiveData()
+    val myPageEvent get() = _myPageEvent
+
+    fun updateSearchedItem(item: ContentData) {
+        _searchedEvent.value = MainSharedEventForSearch.UpdateSearcedContent(item)
+    }
+
+    fun updateMyPAgeItems(items: List<ContentData>?) {
+        items?.filter {
+            it.isLike
+        }?.also {
+            _myPageEvent.value = MainSharedEventForMyPage.UpdateMyPageItem(it)
+        }
+    }
+}
+
+sealed interface MainSharedEventForSearch {
+    data class UpdateSearcedContent(
+        val item: ContentData
+    ): MainSharedEventForSearch
+}
+
+sealed interface MainSharedEventForMyPage {
+    data class UpdateMyPageItem(
+        val items: List<ContentData>
+    ):MainSharedEventForMyPage
 }
