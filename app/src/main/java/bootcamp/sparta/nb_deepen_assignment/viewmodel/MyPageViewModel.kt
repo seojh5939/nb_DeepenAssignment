7c292b2cf1dd4ec027d9e5a7be814917cd1cd39d
SeoJungHan <seojh5939@gmail.com>
2023-09-20 17:23:11 +0900

    [FEAT] : ⚙️ MyPage ViewModel 구

--- app/src/main/java/bootcamp/sparta/nb_deepen_assignment/viewmodel/MyPageViewModel.kt
+++ app/src/main/java/bootcamp/sparta/nb_deepen_assignment/viewmodel/MyPageViewModel.kt
@@ -1,4 +1,38 @@
 package bootcamp.sparta.nb_deepen_assignment.viewmodel
 
-class MyPageViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import bootcamp.sparta.nb_deepen_assignment.model.ContentData
+
+class MyPageViewModel: ViewModel() {
+    private var _list = MutableLiveData<List<ContentData>>()
+    val list get() = _list
+
+    fun addItem(item: ContentData) {
+        val currentList = list.value.orEmpty().toMutableList()
+        currentList.add(item)
+        _list.value = currentList
+    }
+
+    fun removeItem(position: Int? =null, item: ContentData) {
+        fun searchIndex(item: ContentData): Int {
+            val currentList = list.value.orEmpty().toMutableList()
+            val selectedItem = currentList.find { it.id == item.id }
+            return selectedItem?.let { currentList.indexOf(it) } ?: return -1
+        }
+
+        val currentList = list.value.orEmpty().toMutableList()
+        val currentPosition = position ?: searchIndex(item)
+
+        if(currentPosition == -1) {
+            return
+        }
+
+        currentList.removeAt(currentPosition)
+        _list.value = currentList
+    }
+
+    fun updateMyPageItems(items: List<ContentData>) {
+        _list.value = items
+    }
 }
