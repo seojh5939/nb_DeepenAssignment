2b183b16b6ddf2601e156886ceb392ddf33695eb
SeoJungHan <seojh5939@gmail.com>
2023-09-20 17:21:20 +0900

    [FEAT] : ⚙️ MyPage Adapter 좋아요 기능구

--- app/src/main/java/bootcamp/sparta/nb_deepen_assignment/ui/mypage/MyPageListAdapter.kt
+++ app/src/main/java/bootcamp/sparta/nb_deepen_assignment/ui/mypage/MyPageListAdapter.kt
@@ -1,19 +1,17 @@
-package bootcamp.sparta.nb_deepen_assignment.ui.search
+package bootcamp.sparta.nb_deepen_assignment.ui.mypage
 
 import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import bootcamp.sparta.nb_deepen_assignment.databinding.RecyclerItemBinding
+import bootcamp.sparta.nb_deepen_assignment.databinding.MyPageItemBinding
 import bootcamp.sparta.nb_deepen_assignment.model.ContentData
+import bootcamp.sparta.nb_deepen_assignment.utils.Utils.changeDateFormat
 import com.bumptech.glide.Glide
-import java.text.SimpleDateFormat
-import java.util.Locale
 
-class SearchListAdapter(private val context: Context): ListAdapter<ContentData, SearchListAdapter.SearchHolder>(
+class MyPageListAdapter(private val onLikeClickListener: (Int, ContentData) -> Unit): ListAdapter<ContentData, MyPageListAdapter.Holder>(
     object: DiffUtil.ItemCallback<ContentData>() {
         override fun areItemsTheSame(oldItem: ContentData, newItem: ContentData): Boolean {
             return oldItem.id == newItem.id
@@ -25,33 +23,34 @@ class SearchListAdapter(private val context: Context): ListAdapter<ContentData, 
     }
 ) {
 
-    class SearchHolder(private val binding: RecyclerItemBinding, private val context: Context): RecyclerView.ViewHolder(binding.root) {
+    class Holder(
+        private val binding: MyPageItemBinding,
+        private val context: Context,
+        private val onLikeClickListener: (Int, ContentData) -> Unit
+    ): RecyclerView.ViewHolder(binding.root) {
         fun bind(item: ContentData)=with(binding) {
-            Log.d("Adapter", item.dateTime)
-            val old_format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.000Z", Locale.KOREAN)
-            val old_date = old_format.parse(item.dateTime)
-            val formatter = SimpleDateFormat("yyyy-MM-dd", Locale.KOREAN)
-            val new_date = old_date?.let { formatter.format(it) }
-
-            dttm.text = new_date
+            dttmMypage.text = changeDateFormat(item.dateTime)
             Glide.with(context)
                 .load(item.thumbnail)
-                .into(image)
+                .into(imageMypage)
+
+            likeMypage.setOnClickListener {
+                onLikeClickListener(
+                    bindingAdapterPosition,
+                    item.copy(
+                        isLike = false
+                    )
+                )
+            }
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchHolder {
-        val view = RecyclerItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-//        val layoutParams = RecyclerView.LayoutParams(
-//            ViewGroup.LayoutParams.MATCH_PARENT,
-//            ViewGroup.LayoutParams.WRAP_CONTENT
-//        )
-//        parent.rootView.layoutParams = layoutParams
-//        parent.rootView.setPadding(10, 10, 10, 10dp)
-        return SearchHolder(view, context)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = MyPageItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return Holder(view, parent.context, onLikeClickListener)
     }
 
-    override fun onBindViewHolder(holder: SearchHolder, position: Int) {
+    override fun onBindViewHolder(holder: Holder, position: Int) {
         holder.bind(getItem(position))
     }
 }
