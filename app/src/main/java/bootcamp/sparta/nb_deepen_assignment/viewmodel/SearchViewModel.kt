2c58206d6e1cbae55acf40313f4e53a9dba6efff
SeoJungHan <seojh5939@gmail.com>
2023-09-22 00:39:54 +0900

    [REFACTOR] : ♻️ SearchImageAndVideo 구

--- app/src/main/java/bootcamp/sparta/nb_deepen_assignment/viewmodel/SearchViewModel.kt
+++ app/src/main/java/bootcamp/sparta/nb_deepen_assignment/viewmodel/SearchViewModel.kt
@@ -5,23 +5,69 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import bootcamp.sparta.nb_deepen_assignment.model.ContentData
-import bootcamp.sparta.nb_deepen_assignment.model.retrofit.VideoData
 import bootcamp.sparta.nb_deepen_assignment.repository.ContentRepository
 import kotlinx.coroutines.launch
-import java.util.concurrent.atomic.AtomicLong
 
-class SearchViewModel(private val repository: ContentRepository): ViewModel(){
+class SearchViewModel(private val repository: ContentRepository) : ViewModel() {
     private val _list: MutableLiveData<List<ContentData>> = MutableLiveData()
     val list get() = _list
+    var loading: MutableLiveData<Boolean> = MutableLiveData()
+    var query: MutableLiveData<String> = MutableLiveData()
 
-    fun resultImageAndVideo(query: String) {
+    val searchedList = mutableListOf<ContentData>()
+    private var pageableCountImage: Int = 1
+    private var pageableCountVideo: Int = 1
+    private var imgSearchRes = false
+    private var videoSearchRes = false
+
+    fun resultImageAndVideo(query: String, page: Int = 1) {
+        imgSearchRes = false
+        videoSearchRes = false
+        loading.value = true
+
+        // Image
         viewModelScope.launch {
-            val result = repository.resultImageAndVideo(query, "recency")
-            _list.value = result
+            with(repository) {
+                if (page <= pageableCountImage) {
+                    val image = searchImage(query, "recency", page, 40)
+                    with(image) {
+                        if (isSuccessful) {
+                            body()?.let { body ->
+                                body.documents?.let { images -> searchedList.addAll(images.toContentDataList()) }
+                                body.metaData?.pageableCount?.let { count ->
+                                    pageableCountImage = count
+                                }
+                            }
+                        }
+                        imgSearchRes = true
+                        checkResponse()
+                    }
+                }
+            }
+        }
+
+        // Video
+        viewModelScope.launch {
+            with(repository) {
+                val video = searchVideo(query, "recency", page, 40)
+                with(video) {
+                    if (isSuccessful) {
+                        body()?.let { body ->
+                            body.documents?.let { video -> searchedList.addAll(video.toContentDataList()) }
+                            body.metaDAta?.pageableCount?.let { count ->
+                                pageableCountVideo = count
+                            }
+                        }
+                    }
+                }
+                videoSearchRes = true
+                checkResponse()
+            }
         }
     }
 
-    fun modifySearchItem(position: Int?= null, item: ContentData) {
+
+    fun modifySearchItem(position: Int? = null, item: ContentData) {
         fun searchIndex(item: ContentData): Int {
             val currentList = list.value.orEmpty().toMutableList()
             val selectedItem = currentList.find { it.id == item.id }
@@ -32,16 +78,26 @@ class SearchViewModel(private val repository: ContentRepository): ViewModel(){
         val currentList = list.value.orEmpty().toMutableList()
         val currentPosition = position ?: searchIndex(item)
 
-        if(currentPosition == -1) {
+        if (currentPosition == -1) {
             return
         }
 
         currentList[currentPosition] = item
         _list.value = currentList
     }
+
+    private fun checkResponse() {
+        if (imgSearchRes && videoSearchRes) {
+            // sort
+            searchedList.sortWith { o1, o2 -> o2.dateTime.compareTo(o1.dateTime) }
+
+            _list.value = searchedList
+            loading.value = false
+        }
+    }
 }
 
-class MainViewModelFactory(private val repository: ContentRepository): ViewModelProvider.Factory {
+class MainViewModelFactory(private val repository: ContentRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         @Suppress("UNCHECKED_CAST")
         return SearchViewModel(repository) as T
